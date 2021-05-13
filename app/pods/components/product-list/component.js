4f8b6cc9c7bb5354357005f09bc64fe642f4e217
LucasSys <lucas@systemar.com.br>
2021-05-13 16:25:08 +0000

    [fix]

--- app/pods/components/product-list/component.js
+++ app/pods/components/product-list/component.js
@@ -4,7 +4,28 @@ import { inject as service } from '@ember/service';
 export default class ProductListComponent extends Component {
 
     @service() store;
-    
+
+    activeFilter = "all";
+
+    filters = [
+      {
+        title: "Todos",
+        alias: "all"
+      },
+      {
+        title: "Recentes",
+        alias: "recent"
+      },
+      {
+        title: "Brechó",
+        alias: "thrift-store"
+      },
+      {
+        title: "Novo",
+        alias: "no-thrift-store"
+      },
+    ];
+
     get list() {
         return this.store.findAll("product");
     }
